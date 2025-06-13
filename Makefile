961eb97103608309f4f59bb12f4b4daa43fc0fd8
wangtengfei <wtf@xiaoroujiankang.com>
2025-06-13 18:23:29 +0800

    增加多线程安全日志

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ PROJECT_NAME = test
 BUILD_DIR = ./build
 BINARY_FILE = $(BUILD_DIR)/$(PROJECT_NAME)
 SOURCE_DIR = ./src
-INCLUDE_DIR = -I./include -I/Users/wtf/Documents/www/c/util -I/opt/homebrew/opt/boost/include
+INCLUDE_DIR = -I./include -I/Users/wtf/Documents/www/c/util -I/opt/homebrew/opt/boost/include -I/opt/homebrew/opt/glog/include -I/opt/homebrew/opt/gflags/include
 LIB_DIR = -L./lib -L/Users/wtf/Documents/www/c/util -L/opt/homebrew/opt/boost/lib
 CXX=clang++
 CXX_STD = -std=c++20
--- src/coroutine.cpp
+++ src/coroutine.cpp
@@ -1,6 +1,27 @@
 #include "cpp_inc/coroutine_default_sche.h"
 #include <thread>
 #include <iostream>
+#include <glog/logging.h>
+
+void SetupLogging(const std::string& logDir)
+{
+    // 设置日志输出目录
+    FLAGS_log_dir = logDir;
+    // google::SetLogDestination(google::INFO, "./logs/my_log_");
+
+    // 设置日志级别
+    FLAGS_stderrthreshold = google::INFO;
+
+    // 设置日志文件大小为10MB
+    FLAGS_max_log_size = 10 * 1024;  // 10MB
+
+    // 禁用日志文件名中的机器名后缀
+    FLAGS_alsologtostderr = true;
+    FLAGS_logtostderr = false;
+
+    // 自动移除旧日志 day （apt旧版本没有）
+    // google::EnableLogCleaner(3);
+}
 
 //这是一个全局的协程调度器
 util::CoroutineDefaultSche sche;
@@ -12,22 +33,42 @@ util::CoroutinePromise<void> handle(int params)
     //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     //模拟逻辑耗时
     co_await sche.sleepTimeout(params * 1000);
-    std::cout << "coroutine logic: " << params << std::endl;
+    // std::cout << "coroutine logic: " << params << std::endl;
+    LOG(INFO) << "coroutine logic: " << params;
     co_return;
 }
 
 //测试C++20协程
 void test_coroutine()
 {
-    int i = 0;
-    for (; i < 5; i++) {
-        //注册协程任务
-        sche.readyExec(std::bind([](int params) -> util::CoroutinePromise<void>  {
-            co_await handle(params);
-        }, i));
-        std::cout << "readyExec after: " << i << std::endl;
-    };
-    //调度协程
-    while(sche.doSche(2000) == util::CoroutineDefaultSche::ST_RUN);
+    // 设置日志目录
+    std::string logDir = "./logs";
+
+    // 初始化日志
+    SetupLogging(logDir);
+
+    // 初始化 Glog
+    google::InitGoogleLogging("./logs");
+
+    // std::thread t([](){
+
+        int i = 0;
+        for (; i < 5; i++) {
+            //注册协程任务
+            // sche.readyExec(std::bind([](int params) -> util::CoroutinePromise<void>  {
+            //     co_await handle(params);
+            // }, i));
+            sche.readyExec([params = i]() -> util::CoroutinePromise<void> {
+                co_await handle(params);
+            });
+            // std::cout << "readyExec after: " << i << std::endl;
+            LOG(INFO) << "readyExec after: " << i << std::endl;
+        };
+        //调度协程
+        while(sche.doSche(2000) == util::CoroutineDefaultSche::ST_RUN);
+    // });
+    // t.join();
+
+
 }
 
