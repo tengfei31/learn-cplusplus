25275f9a6e16c0a9b7fe44469f611c5c24afa48d
wangtengfei <wtf@xiaoroujiankang.com>
2025-09-16 15:15:25 +0800

    修改tcp_server

--- src/server/tcp_server.cpp
+++ src/server/tcp_server.cpp
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/event.h>
+#include <fcntl.h>
 
 #include "utils/thread_pool.h"
 // #include "util/inc/sysapi/nio.h"
@@ -16,15 +17,21 @@
 
 ThreadPool threadPool(4);
 
-void handle_connection(int clientFd) {
+int handle_connection(int clientFd) {
     char buffer[1024];
-    read(clientFd, buffer, sizeof(buffer));
+    int n = read(clientFd, buffer, sizeof(buffer));
+    if (n <= 0) {
+        return 0;
+    }
     std::cout << "Client message: " << buffer << std::endl;
 
-    std::string msg = "welcome to cpp world\n";
-    write(clientFd, msg.c_str(), msg.length());
+    std::string msg = "welcome to cpp world\n\n\n";
+    int len = write(clientFd, msg.c_str(), msg.length());
 
-    close(clientFd);
+    //如果需要主动断开，或者检测客户端断开连接，可以通过queue队列通知主线程，来主动close和FD_CLR
+    //close(clientFd);
+
+    return len;
 }
 
 
@@ -48,37 +55,47 @@ int tcp_server() {
         return -1;
     }
     //这里增加epoll，来处理多路复用
-
+    fd_set master_set, read_set;
+    FD_ZERO(&master_set);
+    FD_SET(sockFd, &master_set);
+    int maxFd = sockFd;
+    int activity;
 
     while(1) {
-        struct sockaddr_in clientAddr;
-        socklen_t clientAddrLen = sizeof(clientAddr);
-        int clientFd = accept(sockFd, (struct sockaddr*) &clientAddr, &clientAddrLen);
-        if (clientFd < 0) {
-            std::cerr << "Accept failed" << std::endl;
-            continue;
+        read_set = master_set;
+        activity = select(maxFd + 1, &read_set, NULL, NULL, NULL);
+        if (activity < 0) {
+            std::cerr << "select failed" << std::endl;
+            break;
         }
-        //将客户端链接分发到线程池中
-        std::cout << "Client connected: " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) << '\n';
-
-        // threadPool.addTask([](int clientFd){
-        //     char buffer[1024];
-        //     read(clientFd, buffer, sizeof(buffer));
-        //     std::cout << "Client message: " << buffer << std::endl;
-
-        //     std::string msg = "welcome to cpp world\n";
-        //     write(clientFd, msg.c_str(), msg.length());
-
-        //     close(clientFd);
-        // }, clientFd);
-        //or直接使用lambda表达式
-        threadPool.enqueue(handle_connection, clientFd).get();
-        //threadPool.addTask(handle_connection, clientFd);
-
+        //有新的连接
+        if (FD_ISSET(sockFd, &read_set)) {
+            struct sockaddr_in clientAddr;
+            socklen_t clientAddrLen = sizeof(clientAddr);
+            int clientFd = accept(sockFd, (struct sockaddr*) &clientAddr, &clientAddrLen);
+            if (clientFd >= 0) {
+                FD_SET(clientFd, &master_set);
+                if (clientFd > maxFd) {
+                    maxFd = clientFd;
+                }
+                //将客户端链接分发到线程池中
+                std::cout << "Client connected: " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) << '\n';
+            }
+        }
+        //检查所有连接的客户端
+        int fd = 0;
+        for (; fd <= maxFd; fd++) {
+            if (fd != sockFd && FD_ISSET(fd, &read_set)) {
+                //使用work线程池处理业务逻辑
+                threadPool.enqueue(handle_connection, fd);
+                // if (len <= 0) {
+                //     close(fd);
+                //     FD_CLR(fd, &master_set);
+                // }
+            }
+        }
     }
     close(sockFd);
-
-
 
     return 0;
 }
